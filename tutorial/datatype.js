22aad2d16c047ad85ad276156374ca9fb213cf36
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-01-08 11:23:10 +0000

    data types

---
+++ tutorial/datatype.js
@@ -0,0 +1,17 @@
+"use strict" ; // this will tereat all your code as a newer version of javascipt !
+
+// alert ( 3 + 3 ) / we are using node not browser this is why it will not show 6 or alert  it will show error !
+
+let accountNumber = "12" //this is an example of number data type 
+let Name = "Mamun" // this is an example of string data type 
+let isLogedin = true // this is boolean data type
+let state ; // this is undefined 
+let Currency = null // this is empty/standalone data type 
+// bigint , symbol these are some more data types ;
+
+// object /this is most used data type ;
+
+console.log(typeof accountNumber);
+// to know the type of any data ; / but data type of null will come object ;
+
+
