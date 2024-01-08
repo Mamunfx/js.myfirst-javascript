04e8ff6db50b8886e3e62542069848f115767f6b
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-01-08 11:24:40 +0000

    var-const

---
+++ tutorial/var-const.js
@@ -0,0 +1,20 @@
+//Ways to declare constant value and varisable values 
+const accountID = 12345 
+var accountToken = "69"
+let accountName = "Mamun Ali"
+accountType = 'personal account'
+let accountDuty ;
+
+// console.log([accountID, accountToken , accountName, accountType]);
+
+// accountID = 1234567
+accountToken= "70"
+accountName = "Mamun"
+accountType = "personal account"
+accountDuty= "Security"
+
+console.log([accountID,accountToken, accountName, accountType, accountDuty]);
+
+// accountId = 1234567 / Its not allowed because accountId is a constant value !
+// dont use var if the value can be changed in the future ! Use let instead !
+// if you dont assign the value of let or var it will output "undefined" but memory will be allocated for the var or let !
