6beaa553fdc2be2c4b8d171ece9af8b6e805afb1
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-01-09 10:40:07 +0000

    comparison in javascript

---
+++ tutorial/comparison-in.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1); // these are basic comparison in javascript ;
+
+
+// console.log("2" > 1);
+// console.log("02" > 1); // this is confusing comparison ;
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0); // we should ignore all these types of comparison ;
+
+// === 
+
+console.log("2" === 2); // this is strict comparison ;
