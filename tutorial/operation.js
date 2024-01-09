ee858d8fb35f64f7da8595b80ef9aaa63e3f615b
Md. Moonzoor Mamun Munna <155729439+Mamunfx@users.noreply.github.com>
2024-01-09 10:24:42 +0000

    Basic operation and convertion .

---
+++ tutorial/operation.js
@@ -0,0 +1,41 @@
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);  //in this case if we start an operation with string js will count all the operationble value as string ;
+// console.log(1 + 2 + "2");  // but if the string is not in the first place js will only count the string as a string and other as number or etc ;
+
+// console.log( (3 + 4) * 5 % 3);  // write readable code by using more paranthisis ;
+
+// console.log(+true);   // it will return as usual just 1 ;
+// console.log(+""); 
+// same goes for this aswell , as empty string is false or 0 for boolean data type if we try to convert this empty string to boolean it will return 0 ;
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);  // it will first collect the value of gameCounter which is 100 and add 1 then it will return 101 as the finnal value ;
+
+let gameCounter2 = 100
+gameCounter2++;
+console.log(gameCounter2); 
